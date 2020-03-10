3aa35b16a7a826876f39131ef4f34c92bfa95e50
Eric Wolf <ericwolf42@gmail.com>
2020-03-10 09:32:49 +0100

    Add gdp dependency

--- servant-demo.nix
+++ servant-demo.nix
@@ -1,6 +1,6 @@
 { mkDerivation, aeson, base, containers, hpack, lens, rio, scotty
 , servant, servant-client, servant-server, stdenv, stm, tasty
-, tasty-wai, text, uuid, wai, warp, wreq
+, tasty-wai, text, uuid, wai, warp, wreq, gdp
 }:
 mkDerivation {
   pname = "servant-demo";
@@ -10,16 +10,16 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     aeson base containers lens rio scotty servant servant-client
-    servant-server stm tasty tasty-wai text uuid wai warp wreq
+    servant-server stm tasty tasty-wai text uuid wai warp wreq gdp
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     aeson base containers lens rio scotty servant servant-client
-    servant-server stm tasty tasty-wai text uuid wai warp wreq
+    servant-server stm tasty tasty-wai text uuid wai warp wreq gdp
   ];
   testHaskellDepends = [
     aeson base containers lens rio scotty servant servant-client
-    servant-server stm tasty tasty-wai text uuid wai warp wreq
+    servant-server stm tasty tasty-wai text uuid wai warp wreq gdp
   ];
   prePatch = "hpack";
   homepage = "https://github.com/chkl/servant-demo#readme";
